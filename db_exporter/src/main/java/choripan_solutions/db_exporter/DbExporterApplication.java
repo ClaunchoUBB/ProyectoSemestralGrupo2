d92073d22763eaefcd06abd286ed1599a3812dee
Claudio Rodríguez <claudio.rodriguez2201@alumnos.ubiobio.cl>
2025-10-26 14:14:18 -0300

    first commit

---
+++ db_exporter/src/main/java/choripan_solutions/db_exporter/DbExporterApplication.java
@@ -0,0 +1,15 @@
+package choripan_solutions.db_exporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbExporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbExporterApplication.class, args);
+		System.out.println("DB Exporter Application Started");
+		System.out.println("So y sas");
+	}
+
+}
---
+++ db_exporter/src/test/java/choripan_solutions/db_exporter/DbExporterApplicationTests.java
@@ -0,0 +1,13 @@
+package choripan_solutions.db_exporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbExporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
