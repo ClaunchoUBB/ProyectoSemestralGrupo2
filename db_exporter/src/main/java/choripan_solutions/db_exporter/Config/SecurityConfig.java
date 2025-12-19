711a22f863f45c9f0cdb35459aab4b12e69f482c
L3chugas <carlosbenjaminorellana@gmail.comm>
2025-12-19 03:21:56 -0300

    intento arreglo 2

--- db_exporter/src/main/java/choripan_solutions/db_exporter/Config/SecurityConfig.java
+++ db_exporter/src/main/java/choripan_solutions/db_exporter/Config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 import choripan_solutions.db_exporter.Repositorio.UsuarioRepositorio;
 import choripan_solutions.db_exporter.auth.JwtTokenProvider;
@@ -40,6 +41,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.applyPermitDefaultValues();
+                    return config;
+                }))
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
